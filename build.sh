9fa11bad48eb4b7f8d5ebebf8d09d8eb923808b1
Khronion <khronion@gmail.com>
2022-03-23 23:02:22 -0700

    Some build.sh fixes that I didn't catch the first time around.

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ read -p "Do you need to install Ubuntu python3-pip and python3-venv packages? (y
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo "Installing python3-venv and python3-pip..."
+    echo "Installing python3-venv, python3-pip, and upx..."
     echo "You may be asked to enter your password to continue."
-    sudo apt update && sudo apt install -y python3-venv python3-pip
+    sudo apt update && sudo apt install -y python3-venv python3-pip upx
 fi
 
 echo "Creating a virtual environment..."
@@ -22,9 +22,11 @@ echo "Installing build tools..."
 pip install pyinstaller
 
 echo "Building..."
-pyinstaller --clean Spyglass-cli.py -F -n Spyglass -c
+pyinstaller --clean spyglass.py -F -n Spyglass-Executable -c
 mv dist/Spyglass .
 echo "Done!"
 echo "Cleaning up..."
 rm -rf build/
 rm -rf dist/
+
+echo "Done! Run ./Spyglass-Executable to use Spyglass."
