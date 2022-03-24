ca17470051973b2eadcc0e59aa61c54b8d2d6e17
Khronion <khronion@gmail.com>
2022-03-23 22:50:45 -0700

    build.sh updated to provide option to install python3-pip and python3-venv packages for Ubuntu users.

--- build.sh
+++ build.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-echo "Creating virtual environment"
+echo "Creating Spyglass environment..."
+
+read -p "Do you need to install Ubuntu python3-pip and python3-venv packages? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Installing python3-venv and python3-pip..."
+    echo "You may be asked to enter your password to continue."
+    sudo apt update && sudo apt install -y python3-venv python3-pip
+fi
+
+echo "Creating a virtual environment..."
 python3 -m venv venv
 source venv/bin/activate
-echo "Installing requirements"
+
+echo "Installing requirements..."
 pip install -r requirements.txt
-echo "Installing build tools"
+
+echo "Installing build tools..."
 pip install pyinstaller
-echo "Building"
+
+echo "Building..."
 pyinstaller --clean Spyglass-cli.py -F -n Spyglass -c
 mv dist/Spyglass .
-echo "Done"
-echo "Cleaning up"
+echo "Done!"
+echo "Cleaning up..."
 rm -rf build/
 rm -rf dist/
