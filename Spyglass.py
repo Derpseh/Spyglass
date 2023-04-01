aecadd957de9334e4e14fb66800b0936b3215565
AavHRF <65682563+AavHRF@users.noreply.github.com>
2023-03-31 17:42:23 -0700

    Update Spyglass.py

--- Spyglass.py
+++ Spyglass.py
@@ -12,7 +12,7 @@ from typing import List
 from xml.etree import ElementTree
 
 # UPDATE THIS EVERY TIME A NEW RELEASE IS PACKAGED
-VERSION = "1.6-cli"
+VERSION = "remove this pls"
 
 # Spyglass
 # Source code by Derps aka Panzer Vier
