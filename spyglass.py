60847e3351f50c118489208b1eb487d01003d840
esfalsa <pronoun.nationstates@gmail.com>
2023-04-02 01:18:46 -0700

    Bump version number

--- spyglass.py
+++ spyglass.py
@@ -18,7 +18,7 @@ import logging
 # Yay more modifications & V3 rewrite (Aav)
 
 # UPDATE THIS EVERY TIME A NEW RELEASE IS PACKAGED
-VERSION = "3.0.0"
+VERSION = "3.0.1"
 
 # Color declarations because OpenPyXL removed them :^)
 RED = Color(rgb="FFFF0000")
